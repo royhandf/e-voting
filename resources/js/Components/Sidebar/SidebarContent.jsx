87c095750492416708dcc40153fac4695e3e4363
Royhan Daffa <royhandaffa14@gmail.com>
2025-08-07 11:40:24 +0700

    fix: route history

--- resources/js/Components/Sidebar/SidebarContent.jsx
+++ resources/js/Components/Sidebar/SidebarContent.jsx
@@ -44,7 +44,7 @@ const userRoutes = [
     { name: "Pemilihan", path: "/votes", icon: IoCheckboxOutline },
     {
         name: "Riwayat Pemilihan",
-        path: "/votes/history",
+        path: "/history",
         icon: IoTimeOutline,
     },
 ];
